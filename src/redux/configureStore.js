b5c6a38855b7d0416e73cae27b2310351c8cfa66
Alexander Oguzie-Ibeh <harlexibeh01@gmail.com>
2022-03-28 08:49:50 +0100

    add action and reducers for books and categories + setup redux store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { combineReducers, createStore } from 'redux';
+import books from './books/books';
+import categories from './categories/categories';
+
+const rootReducer = combineReducers({
+  books,
+  categories,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
